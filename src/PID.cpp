0c2a9e2ae4d139269fe991fcfd3db0740a4190ba
donele <dxdy@yahoo.com>
2018-01-04 21:16:06 -0600

    add option run_fast. fix bug that confused I and D.

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,5 +1,6 @@
 #include "PID.h"
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -8,14 +9,18 @@ using namespace std;
 */
 
 PID::PID()
-  :pid_err({0., 0., 0.}), // P, I, D
+  :run_fast(false),
+  pid_err({0., 0., 0.}), // P, I, D
+  coeff_throttle(.3),
+  coeff_angle(.1),
   n_err(3),
   prev_p_error(0.),
   lap_err(0.),
   prev_lap_err(0.),
   best_lap_err(0.),
   do_optimize(false),
-  cnt_max(1700)
+  //cnt_max(1700)
+  cnt_max(1300)
 {
 }
 
@@ -29,18 +34,65 @@ void PID::Init(double Kp, double Ki, double Kd) {
     dCoeff.push_back(.01 * c);
 }
 
-void PID::UpdateError(double cte) {
+void PID::RunFast() {
+  run_fast = true;
+}
+
+void PID::UpdateError(double cte, double speed, double angle) {
   pid_err[0] = cte;
-  pid_err[1] = cte - prev_p_error;
+  pid_err[1] += cte;
+  pid_err[2] = cte - prev_p_error;
   prev_p_error = cte;
-  pid_err[2] += cte;
+  current_speed = speed;
+  current_angle = angle;
 }
 
 double PID::TotalError() {
+}
+
+double PID::Steering() {
   if(do_optimize)
     Twiddle();
 
-  return -coeff[0]*pid_err[0] - coeff[1]*pid_err[1] - coeff[2]*pid_err[2];
+  double controlP = 0.;
+  double controlI = 0.;
+  double controlD = 0.;
+
+  if(run_fast) {
+    double speed_factor = (current_speed > 1e-6) ? 40./current_speed : 1.;
+    controlP = - coeff[0]*pid_err[0]*speed_factor;
+    controlI = - coeff[1]*pid_err[1]*speed_factor;
+    controlD = - coeff[2]*pid_err[2]*speed_factor;
+  }
+  else {
+    controlP = - coeff[0]*pid_err[0];
+    controlI = - coeff[1]*pid_err[1];
+    controlD = - coeff[2]*pid_err[2];
+  }
+  return controlP + controlI + controlD;
+}
+
+double PID::Throttle() {
+  if(run_fast) {
+    double speed_factor = (current_speed > 1e-6) ? 40./current_speed : 1.;
+    double controlP = coeff[0]*pid_err[0];
+    double controlI = coeff[1]*pid_err[1];
+    double controlD = coeff[2]*pid_err[2];
+    double throttle = 2. - coeff_throttle * fabs(controlP + controlI + controlD)*current_speed;
+
+    //double reduce = coeff_throttle * pid_err[2];
+    //if(reduce < 0.)
+    //  reduce = 0.;
+    //double throttle = 2. - reduce;
+
+    //double reduce = coeff_angle * fabs(current_angle);
+    //double throttle = 2. - reduce;
+    //cout << "angle " << current_angle << endl;
+    return throttle;
+  }
+  else
+    return 0.3;
+  return 0.;
 }
 
 void PID::DoOptimize() {
