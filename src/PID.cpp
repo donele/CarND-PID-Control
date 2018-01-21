af80316254250956c7752aa2187a0ab1b5521491
donele <dxdy@yahoo.com>
2018-01-20 20:24:02 -0600

    add comments. change var names.

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,7 +10,6 @@ using namespace std;
 
 PID::PID()
   :pid_err({0., 0., 0.}), // P, I, D
-  coeff_throttle(22),
   n_err(3),
   prev_p_err(0.),
   lap_err(0.),
@@ -23,52 +22,55 @@ PID::PID()
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
-  coeff = {Kp, Ki, Kd};
+  K = {Kp, Ki, Kd};
   
   // Twiddle parameters.
-  for(auto c : coeff)
-    dCoeff.push_back(.01 * c);
+  for(auto k : K)
+    dK.push_back(.01 * k);
 }
 
 void PID::UpdateError(double cte, double speed, double angle) {
   pid_err[0] = cte;
-  pid_err[1] += cte;
+
+  if(cte < 1e-6)
+    pid_err[1] = 0.;
+  else
+    pid_err[1] += cte;
+
   pid_err[2] = cte - prev_p_err;
   prev_p_err = cte;
 
   current_speed = speed;
   current_angle = angle;
+
+  if(do_optimize)
+    Twiddle();
 }
 
 double PID::TotalError() {
 }
 
 double PID::Steering() {
-  if(do_optimize)
-    Twiddle();
-
-  double controlP = 0.;
-  double controlI = 0.;
-  double controlD = 0.;
+  double controlP = - K[0] * pid_err[0];
+  double controlI = - K[1] * pid_err[1];
+  double controlD = - K[2] * pid_err[2];
 
   // Reduce steering at high speed.
   double speed_factor = (current_speed > 40) ? 40./current_speed : 1.;
-
-  controlP = - coeff[0]*pid_err[0]*speed_factor;
-  controlI = - coeff[1]*pid_err[1]*speed_factor;
-  controlD = - coeff[2]*pid_err[2]*speed_factor;
-  return controlP + controlI + controlD;
+  double control = (controlP + controlI + controlD) * speed_factor;
+  return control;
 }
 
 double PID::Throttle() {
+  double controlP = K[0] * pid_err[0];
+  double controlI = K[1] * pid_err[1];
+  double controlD = K[2] * pid_err[2];
+
   // Reduce throttle above speed = 40.
   // The amount of reduction is proportinal to the error.
-  double speed_factor = (current_speed > 40) ? current_speed/100. : 0.;
-
-  double controlP = coeff[0]*pid_err[0];
-  double controlI = coeff[1]*pid_err[1];
-  double controlD = coeff[2]*pid_err[2];
-  double throttle = 2. - coeff_throttle * fabs(controlP + controlI + controlD)*speed_factor;
+  const double throttle_max = 2.;
+  double speed_factor = (current_speed > 40) ? current_speed/5. : 0.;
+  double throttle = throttle_max - fabs(controlP + controlI + controlD) * speed_factor;
   return throttle;
 }
 
@@ -78,7 +80,7 @@ void PID::DoOptimize() {
 
 void PID::Twiddle() {
   static int cnt = 0;
-  static int iCoeff = 0;
+  static int iK = 0;
   static TwiddleState state = added;
 
   // Sum of the CTE during lap
@@ -87,41 +89,48 @@ void PID::Twiddle() {
   // Run twiddle after each lap.
   ++cnt;
   if(cnt >= cnt_max) {
-    printf("C %.8f %.8f %.8f err %.2f %.2f\n", coeff[0], coeff[1], coeff[2], best_lap_err, lap_err);
+    printf("C %.8f %.8f %.8f err %.2f %.2f\n", K[0], K[1], K[2], best_lap_err, lap_err);
     cout.flush();
 
+    // First lap. Update the best error and proceed with twiddle.
     if(best_lap_err == 0.) {
       best_lap_err = lap_err;
-      coeff[iCoeff] += dCoeff[iCoeff];
+      K[iK] += dK[iK];
       state = added;
     }
     else {
+      // If dK[iK] was added to K[iK] at the beginning of the current lap.
       if(state == added) {
+        // If lap_err is smaller than the previous best, update the best error.
         if(lap_err < best_lap_err) {
           best_lap_err = lap_err;
-          dCoeff[iCoeff] *= 1.1;
-          iCoeff = (iCoeff + 1) % n_err;
-          coeff[iCoeff] += dCoeff[iCoeff];
+          dK[iK] *= 1.1;
+          iK = (iK + 1) % n_err;
+          K[iK] += dK[iK];
           state = added;
         }
+        // If lap_err is larger than the previous best, subtract dK[iK] from K[iK].
         else {
-          coeff[iCoeff] -= 2.*dCoeff[iCoeff];
+          K[iK] -= 2.*dK[iK];
           state = subtracted;
         }
       }
+      // If dK[iK] was subtracted from K[iK] at the beginning of the current lap.
       else if(state == subtracted) {
+        // If lap_err is smaller than the previous best, update the best error.
         if(lap_err < best_lap_err) {
           best_lap_err = lap_err;
-          dCoeff[iCoeff] *= 1.1;
-          iCoeff = (iCoeff + 1) % n_err;
-          coeff[iCoeff] += dCoeff[iCoeff];
+          dK[iK] *= 1.1;
+          iK = (iK + 1) % n_err;
+          K[iK] += dK[iK];
           state = added;
         }
+        // If lap_err is larger than the previous best, reduce dK[iK].
         else {
-          coeff[iCoeff] += dCoeff[iCoeff];
-          dCoeff[iCoeff] *= 0.9;
-          iCoeff = (iCoeff + 1) % n_err;
-          coeff[iCoeff] += dCoeff[iCoeff];
+          K[iK] += dK[iK];
+          dK[iK] *= 0.9;
+          iK = (iK + 1) % n_err;
+          K[iK] += dK[iK];
           state = added;
         }
       }
